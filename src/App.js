b31f53c07f6479194d83d1885a9375b111004200
Wang <sarah@ekohe.com>
2021-05-05 16:39:42 +0800

    Add products componenet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React from 'react';
+import Products from './components/Products';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header> <a href="/">React Shopping Cart </a></header>
-      <main>Product List</main>
-      <footer>All right is reserved</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products : data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header> <a href="/">React Shopping Cart </a></header>
+        <main>
+          <div className="content">
+            <main className="main"><Products products={this.state.products}/></main>
+          </div>
+          <div className="sidebar"></div>
+        </main>
+        <footer>All right is reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
